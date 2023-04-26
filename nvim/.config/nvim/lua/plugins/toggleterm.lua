66051f6c191050c829f6aa50d154cfcf0f51c74f
Subaan Qasim <subaan.qasim@gmail.com>
2023-04-26 13:56:18 +0100

    add/configure plugins

---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<leader>t]],
+      direction = "horizontal",
+      shade_terminals = false,
+    },
+  },
+}
