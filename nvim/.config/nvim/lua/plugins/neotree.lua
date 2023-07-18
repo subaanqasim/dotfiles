eb9cc1210f8491982d0900e34a7b9f98535b7c20
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-18 11:35:27 +0100

    update neotree config (find_args and keymaps)

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,5 +1,35 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          -- dir = require("lazyvim.util").get_root(),
+          dir = vim.loop.cwd(),
+          position = "current",
+          reveal = true,
+        })
+      end,
+      -- desc = "Explorer NeoTree (root dir)",
+      desc = "Explorer NeoTree (cwd)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          -- dir = vim.loop.cwd()
+          dir = require("lazyvim.util").get_root(),
+        })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+      -- desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
   opts = {
     filesystem = {
       filtered_items = {
@@ -7,10 +37,18 @@ return {
         hide_dotfiles = false,
         hide_gitignored = true,
       },
-      bind_to_cwd = true,
+      bind_to_cwd = false,
       cwd_target = {
         sidebar = "global", -- sidebar is when position = left or right
         current = "none", -- current is when position = current
+      },
+      find_args = { -- you can specify extra args to pass to the find command.
+        fd = {
+          "--exclude",
+          ".git",
+          "--exclude",
+          "node_modules",
+        },
       },
     },
     window = {
