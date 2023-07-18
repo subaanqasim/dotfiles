aea226b7b32406b319cd825483910694a8b80c4a
Subaan Qasim <subaan.qasim@gmail.com>
2023-07-18 11:30:50 +0100

    update cmp keymaps

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -2,6 +2,8 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
+      local cmp = require("cmp")
+
       local function border(hl_name)
         return {
           { "╭", hl_name },
@@ -14,6 +16,20 @@ return {
           { "│", hl_name },
         }
       end
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
 
       opts.window = {
         completion = {
