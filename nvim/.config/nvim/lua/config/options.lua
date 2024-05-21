014ad30ccd7f4e800634b02a2c6fcb50d9a6d841
Subaan Qasim <subaan.qasim@gmail.com>
2024-05-21 20:13:47 +0100

    disable winbar

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.winbar = "%=%m %f"
+-- vim.opt.winbar = "%=%m %f"
 vim.g.material_style = "darker"
 
 -- LazyVim root dir detection
